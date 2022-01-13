aec36afca0f3e2613f5351fc5d3f58e5d9448226
Elroy Toscano <andeltos@gmail.com>
2022-01-13 09:38:55 +0530

    Minor styling changes v2

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
-    './src/**/*.html',
-    './src/**/*.vue',
+    './src/**/*.html',    
     './src/**/*.jsx',
     // etc.
   ],
